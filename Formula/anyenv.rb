bc090c1f0e81cbc11c76ad1c5a65ce51cf7884f6
Ryosuke IWANAGA <me@riywo.com>
2019-01-25 07:13:45 -0800

    Update to 1.0.1

--- Formula/anyenv.rb
+++ Formula/anyenv.rb
@@ -1,23 +1,34 @@
 class Anyenv < Formula
   desc "All in one for **env"
   homepage "https://anyenv.github.io/"
-  url "https://github.com/anyenv/anyenv/archive/v1.0.0.tar.gz"
-  sha256 "f922f3d6f319dcbf36a16f392c9d48c2f6338d0f0cc087ca965a8f1bc12a83a4"
+  url "https://github.com/anyenv/anyenv/archive/v1.0.1.tar.gz"
+  sha256 "5d42ac8748db41b8ec0cb3ad986bb10084e55df0153366403ea458e7a958a1a7"
 
   def install
     prefix.install %w[bin completions libexec]
   end
 
-  def caveats
-    <<~EOS
-      Please add the line below to your profile of shell:
-      eval "$(anyenv init -)"
-      You can initialize install manifests directory by:
-      anyenv install --init [git-url] [git-ref]
-    EOS
-  end
-
   test do
-    shell_output("eval \"$(#{bin}/anyenv init -)\" && anyenv --version")
+    Dir.mktmpdir do |dir|
+      profile = "#{dir}/.profile"
+      File.open(profile, "w") do |f|
+        content = <<~EOS
+          export ANYENV_ROOT=#{dir}/anyenv
+          export ANYENV_DEFINITION_ROOT=#{dir}/anyenv-install
+          eval "$(anyenv init -)"
+        EOS
+        f.write(content)
+      end
+
+      cmds = <<~EOS
+        anyenv install --force-init
+        anyenv install --list
+        anyenv install rbenv
+        rbenv install --list
+      EOS
+      cmds.split("\n").each do |cmd|
+        shell_output("source #{profile} && #{cmd}")
+      end
+    end
   end
 end
